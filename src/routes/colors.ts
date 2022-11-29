55916ce41a59a27a34a203bc164304f7d1c59232
Artem Hulko <agulko96@gmail.com>
2022-11-29 22:10:57 +0200

    minor changes in server file

--- src/routes/colors.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import * as colorsController from '../controllers/colors';
-
-export const router = Router();
-
-router.get('/', colorsController.getAll);
-router.get('/:colorId', colorsController.getOne);
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,5 @@
 import express from 'express';
 import cors from 'cors';
-import * as goodController from "./controllers/goods";
-import * as colorsController from "./controllers/colors";
 import serverless from 'serverless-http';
 
 const router = express.Router();
