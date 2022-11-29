4baa3166c1a751caedf66add798fc87e11cad3fa
Artem Hulko <63102835+ArtemHulkoDev@users.noreply.github.com>
2022-11-29 22:08:06 +0200

    Delete src/routes directory

--- src/routes/goods.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import * as goodController from '../controllers/goods';
-
-export const router = express.Router();
-
-router.get('/', goodController.getAll)
-router.get('/:goodId', goodController.getOne)
-router.post('/', goodController.add);
-router.delete('/:goodId', goodController.remove);
